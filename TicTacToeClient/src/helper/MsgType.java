f385e285118b57834812e5e1ea1f425f46b9f5e6
Sahar Nasser <saharnasser150@gmail.com>
2023-01-02 01:01:07 +0200

    Project Skeleton Upload

---
+++ TicTacToeClient/src/helper/MsgType.java
@@ -0,0 +1,13 @@
+package helper;
+
+public class MsgType {
+
+static final int SEND_REQUEST=0;
+static final int DATABASECONNECTION = 1;
+static final int SEND_MOVE=2;
+static final int lIST_AVAILABLE =3;  
+static final int GAME_ENDED =4;
+static final int QUIT_GAME =5; 
+static final int CANCEL_REQ =6; 
+static final int CONFIRM_REQ =7; 
+}
---
+++ TicTacToeServer/src/helper/QueryType.java
@@ -0,0 +1,9 @@
+
+package helper;
+
+public class QueryType {
+    static final int LOGIN=0;
+    static final int SIGNUP=1;
+    static final int GET_RECORD=2;
+    static final int ADD_RECORD=3;
+}
