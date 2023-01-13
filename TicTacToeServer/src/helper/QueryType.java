a5f5a1210de5759b0dc076ba7400ee4456fef47c
Ammar El-Riyali <ammarelriyali@gmail.com>
2023-01-13 20:36:20 +0200

    commit

--- TicTacToeServer/src/helper/QueryType.java
+++ TicTacToeServer/src/helper/QueryType.java
@@ -2,8 +2,11 @@
 package helper;
 
 public class QueryType {
-    static final int LOGIN=0;
-    static final int SIGNUP=1;
-    static final int GET_RECORD=2;
-    static final int ADD_RECORD=3;
+    static final int LOGIN = 0;
+    static final int SIGNUP = 1;
+    static final int GET_RECORD = 2;
+    static final int ADD_RECORD = 3;
+
+    public QueryType() {
+    }
 }
