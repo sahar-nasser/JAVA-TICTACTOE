014382050f2400cf7ad646b02f9b8661441c019b
Sahar Nasser <saharnasser150@gmail.com>
2023-01-13 21:18:52 +0200

    adding skeleton

--- TicTacToeClient/src/replayrecord/RecordsCreator.java
+++ TicTacToeClient/src/replayrecord/RecordsCreator.java
@@ -2,4 +2,10 @@ package replayrecord;
 
 public class RecordsCreator {
     //this class should change the string into a list of objects records
+    private String recordList;
+
+    public void setRecordList(String recordList) {
+        this.recordList = recordList;
+    }
+    
 }
