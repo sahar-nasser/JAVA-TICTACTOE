82922b6c64231b4c79f131c0df4d27d3159337dc
Clara Mounir Adly <claramounir555@gmail.com>
2023-01-13 19:05:35 +0200

    sign up screen

--- TicTacToeServer/src/dataaccesslayer/DataAccessLayer.java
+++ TicTacToeServer/src/dataaccesslayer/DataAccessLayer.java
@@ -1,9 +1,7 @@
 package dataaccesslayer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+
 public class DataAccessLayer {
 
 
@@ -14,9 +12,36 @@ public class DataAccessLayer {
         {
             try {
                 DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
-                con= DriverManager.getConnection("jdbc:derby://localhost:1527/database","root","root");
+                con= DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToe","root","root");
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
         }
+    public  static void addPlayer(Player player) {
+        final String INSERT_CONTACT_SQL = "INSERT INTO record" +
+                "  (username, password, score) VALUES " +
+                " (?, ?, ?, ?)";
+
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:derby://localhost:1527/TicTacToe", "root", "root");
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CONTACT_SQL)) {
+            preparedStatement.setString(1, player.getUsername());
+            preparedStatement.setString(2, player.getPassword());
+            preparedStatement.setInt(3, player.getScore());
+            preparedStatement.set(4, player.getLName());
+            preparedStatement.setString(6, player.getMName());
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            int result = preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            // print SQL exception information
+            printSQLException(e);
+        }
+    }
+
 }
